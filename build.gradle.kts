7cf34be5a8c4bc243faeeba13ca4a2e187c2c0b5
coding meet <codingmeet26@gmail.com>
2023-10-07 18:42:55 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+   kotlin("jvm") version "1.9.0"
+   id("org.jetbrains.compose") version "1.5.0"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    // Note, if you develop a library, you should use compose.desktop.common.
+    // compose.desktop.currentOs should be used in launcher-sourceSet
+    // (in a separate module for demo project and in testMain).
+    // With compose.desktop.common you will also lose @Preview functionality
+    implementation(compose.desktop.currentOs)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+    }
+}
---
+++ src/main/kotlin/theme/Color.kt
@@ -0,0 +1,7 @@
+package theme
+
+import androidx.compose.ui.graphics.Color
+
+val PurpleBlue = Color(0xFF7F52FF)
+val Background = Color(0xFF1F1D2B)
+val TextFieldBackground = Color(0xFF2D2A3D)
