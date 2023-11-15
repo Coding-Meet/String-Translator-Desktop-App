04f6a07b4560a668f994d1f593aa971c8b29cda4
coding meet <codingmeet26@gmail.com>
2023-11-15 16:17:48 +0530

    gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
    kotlin("jvm") version "1.9.0"
    id("org.jetbrains.compose") version "1.5.0"
 }
 
-repositories {
-    mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    google()
-}
 
 dependencies {
     // Note, if you develop a library, you should use compose.desktop.common.
@@ -15,10 +13,19 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+    implementation(compose.runtime)
+    implementation(compose.foundation)
+    implementation(compose.material)
 }
 
 compose.desktop {
     application {
         mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "com.coding.meet.translate"
+            packageVersion = "1.0.0"
+        }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,20 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 pluginManagement {
     repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
         gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
-rootProject.name = "String Translator App"
+rootProject.name = "StringTranslatorApp"
